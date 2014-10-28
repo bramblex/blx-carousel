474a52775c3b5ecb9ce61b003dd9cac4afd4a0e3
brambles <qjnight@gmail.com>
2014-10-28 12:39:37 +0800

    最后加入微信分享的东西

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function(){
+  var data = {
+    'img': 'http://bramblex.github.io/blx-carousel/img/logo.png',
+    'link': 'http://bramblex.github.io/blx-carousel/',
+    'desc': '由华工软宣开发的强大的HTML5电子邀请函框架',
+    'title': 'BlxCarousel.js'
+  };
+
+  var callback = function(){};
+
+  wechat('timeline', data, callback);
+})()
