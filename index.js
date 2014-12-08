f019e05442ac6f48e6a961d9fbd10c05d0d3640f
brambles <qjnight@gmail.com>
2014-12-08 22:03:26 +0800

    add branch animation for BlxAnimate.

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 
-
 BlxAnimate('#loading-logo')
+  .excute(function(_this){
+    console.log('执行');
+    _this.next();
+  })
   .animate('shake')
+  .branch(
+
+    BlxAnimate('#loading-page')
+      .excute(function(_this){
+        console.log('分支执行');
+        _this.next();
+      })
+      .delay(1000)
+      .animate('shake')
+      .animate('tada')
+      .excute(function(_this){
+        console.log('分支结束');
+        _this.next();
+      })
+
+  )
   .animate('tada')
   .delay(1000)
   .animate('wobble')
   .animate('shake')
-  .run();
-
+  .excute(function(_this){
+    console.log('结束');
+    _this.next();
+  })
+  .run()
